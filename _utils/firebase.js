4879251776cc93f8bb7c6ed356f8a956decd1413
jasjot190 <jasjot.singh@edu.sait.ca>
2024-12-07 07:15:53 -0700

    Basic functionality Completed

--- _utils/firebase.js
+++ _utils/firebase.js
@@ -1,9 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+var initializeApp = require("firebase/app").initializeApp;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+
+// import { getFirestore } from "firebase/firestore";
+var getFirestore = require("firebase/firestore").getFirestore;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+const db = getFirestore(app);
+
+module.exports = { db };
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
 
@@ -7,9 +9,74 @@ const port = 5500;
 
 app.use(cors());
 app.use(express.json());
+const db = require("./_utils/firebase").db;
+const {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  query,
+  doc,
+  setDoc,
+} = require("firebase/firestore");
+
+// const getAllCourses = async () => {
+//   let itemsCollectionRef = collection(db, "courses");
+//   let itemsSnapshot = await getDocs(itemsCollectionRef);
+//   let items = itemsSnapshot.docs.map((doc) => ({
+//     id: doc.id,
+//     ...doc.data(),
+//   }));
+//   return items;
+// };
+
+const getAllCourses = async () => {
+  try {
+    let itemsCollectionRef = collection(db, "courses");
+    let itemsSnapshot = await getDocs(itemsCollectionRef);
+    let items = itemsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return items;
+  } catch (error) {
+    console.error("Error fetching courses: ", error);
+    throw error;
+  }
+};
+
+const getAllLessons = async (courseId) => {
+  try {
+    let itemsCollectionRef = collection(
+      doc(db, "courses", courseId),
+      "Lessons"
+    );
+    let itemsSnapshot = await getDocs(itemsCollectionRef);
+    let items = itemsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return items;
+  } catch (error) {
+    console.error("Error fetching Lessons: ", error);
+    throw error;
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to JasjotsBlog Backend!</h1>");
+});
+
+app.get("/courses", async (req, res) => {
+  let recievedItems = await getAllCourses();
+
+  res.send(recievedItems);
+});
+
+app.get("/lessons", async (req, res) => {
+  let Id = req.query.courseId;
+  let recievedItems = await getAllLessons(Id);
+  res.send(recievedItems);
 });
 
 const start = async () => {
