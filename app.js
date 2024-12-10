c82ed2f27d686b48fd3172becdabdd0cbf8c0141
jasjot190 <jasjot.singh@edu.sait.ca>
2024-12-10 13:25:43 -0700

    Main Functionality implemented

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const db = require("./_utils/firebase").db;
 const {
   collection,
   getDocs,
+  getDoc,
   addDoc,
   deleteDoc,
   query,
@@ -63,6 +64,65 @@ const getAllLessons = async (courseId) => {
   }
 };
 
+const getAllTestimonials = async () => {
+  try {
+    let itemsCollectionRef = collection(db, "testimonials");
+    let itemsSnapshot = await getDocs(itemsCollectionRef);
+    let items = itemsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return items;
+  } catch (error) {
+    console.error("Error fetching testimonials: ", error);
+    throw error;
+  }
+};
+
+const getUserInfo = async (userId) => {
+  try {
+    console.log(userId);
+    let itemsCollectionRef = doc(db, "users", userId);
+    let itemsSnapshot = await getDoc(itemsCollectionRef);
+    let items = { id: itemsSnapshot.id, ...itemsSnapshot.data() };
+    return items;
+  } catch (error) {
+    console.error("Error fetching UserInfo: ", error);
+    throw error;
+  }
+};
+const getOptedCourses = async (userId) => {
+  try {
+    let itemsCollectionRef = collection(
+      doc(db, "users", userId),
+      "OptedCourses"
+    );
+    let itemsSnapshot = await getDocs(itemsCollectionRef);
+    let items = itemsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return items;
+  } catch (error) {
+    console.error("Error fetching UserInfo: ", error);
+    throw error;
+  }
+};
+
+const createTestimonial = async (userName, desc) => {
+  try {
+    let itemsCollectionRef = collection(db, "testimonials");
+    let newItem = {
+      name: userName,
+      description: desc,
+    };
+    await addDoc(itemsCollectionRef, newItem);
+  } catch (error) {
+    console.error("Error fetching courses: ", error);
+    throw error;
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to JasjotsBlog Backend!</h1>");
 });
@@ -73,10 +133,34 @@ app.get("/courses", async (req, res) => {
   res.send(recievedItems);
 });
 
-app.get("/lessons", async (req, res) => {
+app.get("/courses/lessons", async (req, res) => {
   let Id = req.query.courseId;
   let recievedItems = await getAllLessons(Id);
   res.send(recievedItems);
+});
+
+app.get("/testimonials", async (req, res) => {
+  let recievedItems = await getAllTestimonials();
+  res.send(recievedItems);
+});
+
+app.get("/users/userInfo", async (req, res) => {
+  let Id = req.query.userId;
+  let recievedItems = await getUserInfo(Id);
+  res.send(recievedItems);
+});
+
+app.get("/users/optedCourses", async (req, res) => {
+  let userId = req.query.userId;
+  let recievedItems = await getOptedCourses(userId);
+  res.send(recievedItems);
+});
+
+app.post("/addTestimonial", async (req, res) => {
+  let userName = req.query.userName;
+  let desc = req.query.description;
+  await createTestimonial(userName, desc);
+  res.send("Added to database");
 });
 
 const start = async () => {
